793873424fd35e7f3ab0c769c8d3c43e18213f39
repl.it user <replituser@example.com>
2020-11-19 08:26:27 +0000

    updating Exc2.cc

--- Exc2.cc
+++ Exc2.cc
@@ -1,8 +1,9 @@
-// fstream for reading "daten.txt" and writing 
-// the sums in "datensumme.txt"
+// fstream and iostream for reading "daten.txt" and writing 
+// the sums in "datensumme.txt" and results to the terminal
 #include <fstream>
 #include <iostream>
 
+#include <cmath>
 
 using namespace std;
 // Main entry point
@@ -26,22 +27,58 @@ int main()
     fin >> a;
     fin >> b;
 
+    if (fin.fail())
+    {
+      break;
+    }
+
     sum+=a;
     sum+=b;//add up everything    
     NTot++;//this should add to 234 ai for normalisation.
 
-    cout<<"a "<< a<<"B "<<b<<endl;
+    //cout << "a: "<< a << ", b: " << b << endl;
 
     // Write the resulting sum into fout
-    // fout << a + b << std::endl;
+    fout << a + b << std::endl;
   }
 
   double mean=sum/NTot;
   
-  cout<<"sum "<<sum<<" ntot "<<NTot<<" mean "<<mean<<std::endl;
+  cout << "Sum: " << sum << "; ntot: "<<NTot<<" mean "<<mean<<std::endl;
+
   // Clean up
   fin.close();
   fout.close();
+
+  // Open daten
+  std::ifstream datensumme("datensumme.txt");
+  double var = 0.f;
+  while (!datensumme.eof())
+  {
+    double val;
+    datensumme >> val;
+
+    if (datensumme.fail())
+    {
+      break;
+    }
+
+    var += (val - mean) * (val - mean);
+  }
+  var /= NTot;
+  cout << "Var: " << var << endl;
+  double sigma = sqrt(var);
+  cout << "sigma: " << sigma << endl;
+
+  datensumme.close();
+
+  // Exercise 2
+  ifstream a("datensumme.txt");
+  ifstream b("datensumme.txt");
+
+  size_t counter = 0;
+  
+
 
   // Ciao
   return 0;
